3e03ac0330d13d4d56d38a8ffa23e1b3c6907a11
Christian Koestlin <christian.koestlin@esrlabs.com>
2018-01-15 21:16:41 +0000

    Use androidlogger for logging

--- source/app.d
+++ source/app.d
@@ -7,23 +7,29 @@ import std.algorithm;
 import std.experimental.logger;
 import std.stdio;
 import std.string;
+import androidlogger;
 
 int main(string[] args)
 {
+    sharedLog = new AndroidLogger(true, LogLevel.all);
+
+    // dfmt off
     auto projectSetups = [
-        new ps.dlang.DDoxPS, new ps.dlang.RakeFormatPS, new ps.dlang.LicenseCommentPS
+        new ps.dlang.DDoxPS,
+        new ps.dlang.RakeFormatPS,
+        new ps.dlang.LicenseCommentPS,
     ];
+    // dfmt on
 
     auto applicable = projectSetups.filter!(a => a.applicable);
     foreach (projectSetup; applicable)
     {
-        "Checking %s".format(projectSetup).info;
+        "main:Checking %s".format(projectSetup).info;
         if (!projectSetup.check)
         {
-            "Running %s".format(projectSetup).info;
+            "main:Running %s".format(projectSetup).info;
             projectSetup.doSetup;
         }
-
     }
 
     return 0;
