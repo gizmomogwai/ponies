9ec86a6b3f35ee1c75c47e5890b18a9881293c3e
Christian Koestlin <christian.koestlin@esrlabs.com>
2018-01-19 14:46:45 +0000

    Add asciitable

---
+++ source/asciitable.d
@@ -0,0 +1,44 @@
+module asciitable;
+
+struct Row {
+    string[] columns;
+    this(string[] data) {
+        this.columns = data;
+    }
+}
+struct AsciiTable {
+    int[] widths;
+    Row[] rows;
+    this(W...)(W widths) {
+        this.widths = [widths];
+    }
+    AsciiTable add(V...)(V values) {
+        rows ~= Row([values]);
+        return this;
+    }
+    string toString(string linePrefix="") {
+        import std.string;
+        string res = "";
+        foreach (row; rows) {
+            if (res.length > 0) {
+                res ~= "\n";
+            }
+            res ~= linePrefix;
+            foreach (idx, column; row.columns) {
+                res ~= leftJustify(column, widths[idx], ' ');
+            }
+        }
+        return res;
+    }
+}
+
+@("asciitable") unittest {
+    import unit_threaded;
+
+    auto table = AsciiTable(10, 3, 5);
+    table.add("1", "2", "3");
+    table.add("4", "5", "6");
+    table.toString.shouldEqual(
+                               "1         2  3    \n" ~
+                               "4         5  6    \n");
+}
