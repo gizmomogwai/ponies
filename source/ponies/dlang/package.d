9a5ec781206dadc0d7ff6122b24054c1b19eb7f2
Christian Koestlin <christian.koestlin@gmail.com>
2021-05-15 22:57:59 +0200

    Before running ponies

--- source/ponies/dlang/package.d
+++ source/ponies/dlang/package.d
@@ -412,6 +412,7 @@ class AddPackageVersionPony : DlangPony
     auto sourcePaths = "sourcePaths \"source\" \"out/generated/packageversion\"\n";
     auto importPaths = "importPaths \"source\" \"out/generated/packageversion\"\n";
     auto dubFetchPackageVersion = "dub fetch packageversion";
+    auto packageVersionDependency = "depdendency \"packageversion\" version=";
     auto addPackageVersionDependency = "dependency \"packageversion\" version=\"~>0.0.17\"\n";
     this()
     {
@@ -464,7 +465,7 @@ class AddPackageVersionPony : DlangPony
             content ~= preGenerateCommands;
         }
 
-        if (!content.canFind(addPackageVersionDependency))
+        if (!content.canFind(packageVersionDependency))
         {
             "Adding packageversion dependency to %s".format(dubSdl).info;
             content ~= addPackageVersionDependency;
